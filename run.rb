57e580daa31aa5dc102e00ea30af6d9b3bd64300
febiasm <trevorfebias@gmail.com>
2023-02-20 21:04:53 +0300

    finalized run.rb code

--- run.rb
+++ run.rb
@@ -12,24 +12,30 @@ second_author = Author.new("david okumu")
 second_magazine = Magazine.new("big Ö", "math ")
 second_article = Article.new(second_author, second_magazine, "big plans")
 
-puts "list of author #{Author.all}"
+third_author = Author.new("Mary Jane")
+third_magazine = Magazine.new("The East African", "Business")
+third_article = Article.new(third_author, third_magazine, "Kenya's economic outlook for 2023")
 
-# puts Author.all
-# puts Magazine.all
-# puts Article.all
+fourth_author = Author.new("Peter Parker")
+fourth_magazine = Magazine.new("Safari", "Travel")
+fourth_article = Article.new(fourth_author, fourth_magazine, "10 hidden gems to explore in Kenya's national parks")
 
-# puts article2.author.name
-# puts article2.magazine.name
-# puts article2.title
+# puts "List of authors: #{Author.all}"
+# puts "All magazines: #{Magazine.all}"
+# puts "All articles: #{Article.all}"
 
-# puts author1.articles
-# puts author1.magazines
-# puts magazine1.contributors
+# puts "Article author: #{second_article.author.name}"
+# puts "Article magazine: #{second_article.magazine.name}"
+# puts "Article title: #{second_article.title}"
 
-# author1.add_article(magazine2, "Exploring the Rainforest")
-# puts author1.articles
-# puts first_author.topic_areas
+# puts "Author articles: #{second_author.articles}"
+# puts "Author magazines: #{second_author.magazines}"
+# puts "Magazine contributors: #{first_magazine.contributors}"
 
-# puts Magazine.find_by_name("big Ö")
-# puts second_magazine.article_titles
-# puts magazine2.contributing_authors
+# second_author.add_article(second_magazine, "The Moringa School")
+# puts "Author articles after adding a new article: #{second_author.articles}"
+# puts "Author topic areas: #{first_author.topic_areas}"
+
+# puts "Magazine found by name: #{Magazine.find_by_name("big Ö").name}"
+# puts "Magazine article titles: #{second_magazine.article_titles}"
+# puts "Magazine contributing authors: #{second_magazine.contributing_authors}"
